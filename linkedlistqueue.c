bac2dc50a01bce318992461ab6ee8c84e16e027c
Allen John <87759887+AllenJohnn@users.noreply.github.com>
2025-10-17 11:33:46 +0530

    linkedlistqueue.c

---
+++ linkedlistqueue.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+ 
+struct node *new, *head, *l, *n, *t;
+int count=0;
+void create()
+{
+	new =(struct node*)malloc(sizeof(struct node));
+	printf("Enter the value : ");
+	int x;
+	scanf("%d",&x);
+	new->data=x;
+	new->next=NULL;
+	count++;
+}                                                                                                                                             
+
+void enqueue()
+{
+	create();
+	if(head==NULL)
+	{
+		head=new;
+	}
+	else
+	{
+		l=head;
+		while(l->next!=NULL)
+		{
+			l=l->next;
+		}
+		l->next=new;
+	}
+}
+
+
+
+void display()
+{
+	if(head==NULL)
+	{
+		printf("Empty Linked List");
+	}
+	else
+	{
+		l=head;
+		while(l!=NULL)
+		{
+			printf("%d\n",l->data);
+			l=l->next;
+		}
+	}
+}
+
+void dequeue()
+{
+	if(head==NULL)
+	{
+		printf("Linked List is empty");
+	}
+	else
+	{
+		t=head;
+		head=head->next;
+		count--;
+	}
+}
+
+
+void main()
+{
+	int ch;
+	while(ch!=4)
+	{
+		printf("\n\tMENU\n----------------------------\n1)Enqueue\n2)Dequeue\n3)Display\n4)Exit\nEnter your choice : ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				enqueue();
+				break;
+			case 2:
+				dequeue();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				break;
+			default:
+				printf("Invalid choice");
+		}
+	}
+}	

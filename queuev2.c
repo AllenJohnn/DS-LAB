9dd50c7aff6967b99344ec3f53789820a41aea2d
Allen John <87759887+AllenJohnn@users.noreply.github.com>
2025-10-20 22:45:55 +0530

    queue_updated

--- queuev2.c
+++ queuev2.c
@@ -1,129 +1,143 @@
-#include<stdio.h>
-int front=-1,rear=-1,size=0;
-int QUEUE[100];
+#include <stdio.h>
+#include <stdlib.h>
 
-int is_full()
-	{
-	if(rear==size-1)
-		{
-		printf("Queue is full");
-		return 1;
-		}
-	return 0;	
-	}
+int queue[5];
+int front = -1, rear = -1;
 
+void enqueue()
+{
+    if (rear == 4)
+    {
+        printf("Queue is full!!\n");
+    }
+    else
+    {
+        int value;
+        printf("Enter the element to enqueue: ");
+        scanf("%d", &value);
 
-	
-int is_empty()
-	{
-	if(front==-1){
-		printf("Queue is empty");
-		return 1;
-		}
-	return 0;
-	}
-		
-void peak()
-	{
-	if(is_empty()==1)
-		return;
-	printf("Peaked item is %d\n",QUEUE[front]);
-	}
+        if (front == -1)
+        {
+            front = 0;
+        }
+        rear++;
+        queue[rear] = value;
 
-//dequeue
+        printf("Value %d has been inserted.\n", value);
+    }
+}
+
 void dequeue()
-	{
-	if(is_empty()==1)
-		return;
-	printf("Removed item is %d\n",QUEUE[front]);
-	if(front==rear)
-	 front = rear = -1;
-	 else
-		front++;
-	}
-	
-//enqueue	
-void enqueue(int val){
-	if(is_full()==1)
-		return;
-	if(front==-1 && rear==-1)
-		front=rear=0;
-	else
-		rear++;
-	QUEUE[rear]=val;
-	}
-void reinit()
-	{
-	if(rear==size-1 && front > 0)
-		{
-		int front2=front,rear2=rear;
-		rear=front=-1;
-		for(int i=front2;rear2>=i;i++)
-		{
-		enqueue(QUEUE[i]);
-		}
-		}	
-	}	
-//display
+{
+    if (front == -1)
+    {
+        printf("Queue is empty!!\n");
+    }
+    else
+    {
+        printf("Removed element is %d\n", queue[front]);
+        if (front == rear)
+        {
+            front = rear = -1; // reset when queue becomes empty
+        }
+        else
+        {
+            front++;
+        }
+    }
+}
+
 void display()
-	{
-	int front2;
-	if(front==-1)
-		{
-		printf("Queue is empty");
-		return;
-		}
-	for(front2=front;rear>=front2;front2++)
-		{
-		printf("%d\n",QUEUE[front2]);
-		}
-	}
-	
+{
+    if (front == -1)
+    {
+        printf("Queue is empty!\n");
+    }
+    else
+    {
+        printf("Queue elements are: ");
+        for (int i = front; i <= rear; i++)
+        {
+            printf("%d\t", queue[i]);
+        }
+        printf("\n");
+    }
+}
 
-void main()
-	{
-	int op,val;
-	while(1){
-		printf("Enter the size of the Queue : ");
-		scanf("%d",&size);
-		if(size>0 && 100>size)
-	 		break;
-		else
-			printf("Invalid Value\n");
-		}
-	while(op!=7){
-		printf("\n\tMENU\n------------------------------------\n1)Enqueue\n2)Dequeue\n3)Display\n4)Peak\n5)Is Full\n6)Is Empty\n7)Exit\nEnter your choice : ");
-		scanf("%d",&op);
-	switch(op)
-	{
-	case 1:
-		printf("Enter the value : ");
-		scanf("%d",&val);
-		reinit();
-		enqueue(val);
-		break;
-	case 2:
-		dequeue();
-		break;
-	case 3:
-		display();
-		break;
-	case 4:
-		peak();
-		break;
-	case 5:
-		if(is_full()==0)
-			printf("Queue is Not Full");
-		break;
-	case 6:
-		if(is_empty()==0)
-			printf("Queue is Not Empty");
-		break;
-	case 7:
-		break;
-	default:
-		printf("Invalid choice");
-	}
-	}
-	}
+void peek()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty!!\n");
+    }
+    else
+    {
+        printf("Front element is %d\n", queue[front]);
+    }
+}
 
+void isfull()
+{
+    if (rear == 4)
+    {
+        printf("Queue is full.\n");
+    }
+    else
+    {
+        printf("Queue is not full.\n");
+    }
+}
+
+void isempty()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty.\n");
+    }
+    else
+    {
+        printf("Queue is not empty.\n");
+    }
+}
+
+int main()
+{
+    int ch;
+
+    while (1)
+    {
+        printf("\n--- Queue Menu ---\n");
+        printf("1) Enqueue\n2) Dequeue\n3) Display\n4) Peek\n5) Is Full\n6) Is Empty\n7) Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            peek();
+            break;
+        case 5:
+            isfull();
+            break;
+        case 6:
+            isempty();
+            break;
+        case 7:
+            printf("Exiting program.\n");
+            exit(0);
+        default:
+            printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}

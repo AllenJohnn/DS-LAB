8f86c1f2343f34492fa69c9594e44def8ad4ffff
Allen John <87759887+AllenJohnn@users.noreply.github.com>
2025-10-27 01:21:59 +0530

    Refactor bitstring operations and improve structure

--- bitstring.c
+++ bitstring.c
@@ -1,47 +1,86 @@
 #include <stdio.h>
 
-int main()
+int a[10]={0,0,0,0,0,0,0,0,0,0};
+int b[10]={0,0,0,0,0,0,0,0,0,0};
+
+
+void uni()
+{ 
+printf("Union is");
+for(int I=1;I<=10;I++)
 {
-    int A[10],B[10];
-    int U[10],I[10],CA[10],CB[10];
-    int i;
-    
-    
-    printf("Enter The Bit To Set A: ");
-    for(i=0;i<10;i++)
-    scanf("%d",&A[i]);
-    printf("\n");
-    
-    printf("Enter The Bit To Set B: ");
-    for (i=0;i<10;i++)
-    scanf("%d",&B[i]);
-    printf("\n");
-    
-    for(i=0;i<10;i++)
-    {
-        U[i] = (A[i] | B[i]);
-        I[i] = (A[i] & B[i]);
-        CA[i] = !A[i];
-        CB[i] = !B[i];
-    }
-    
-     printf("The Intersection Is: ");
-    for(i = 0; i < 10; i++)
-        printf("%d ", I[i]);
-    printf("\n");
-
-    printf("The Union Is: ");
-    for(i = 0; i < 10; i++)
-        printf("%d ", U[i]);
-    printf("\n");
-
-    printf("The Complement Of Set A Is: ");
-    for(i = 0; i < 10; i++)
-        printf("%d ", CA[i]);
-    printf("\n");
-
-    printf("The Complement Of Set B Is: ");
-    for(i = 0; i < 10; i++)
-        printf("%d ", CB[i]);
-    printf("\n");
+printf("%d",a[I]|b[I]);
 }
+}
+
+void inter()
+{
+printf("Intersection is");
+for(int I=1;I<=10;I++)
+{
+printf("%d",a[I]&b[I]);
+}
+}
+
+void  comp()
+{
+printf("Complement of first array is");
+for(int I=1;I<=10;I++)
+{
+ printf("%d",!a[I]);
+}
+printf("Complement of second array is");
+for(int I=1;I<=10;I++)
+{
+ printf("%d",!b[I]);
+}
+}
+
+void main()
+{
+int m, n, I, x, y, ch;
+printf("Enter the size of first array:");
+scanf("%d",&m);
+printf("Enter the elements of first array:");
+for(I=0;I<m;I++)
+{
+scanf("%d",&x);
+a[x]=1;
+}
+printf("Bitstring of first array is:");
+for(I=1;I<=10;I++)
+{
+printf("%d",a[I]);
+}
+printf("\nEnter the size of second array:");
+scanf("%d",&n);
+printf("Enter the elements of second array:");
+for(I=0;I<n;I++)
+{
+scanf("%d",&y);
+b[y]=1;
+}
+printf("\nBitstring of second array is:");
+for(I=1;I<=10;I++)
+{
+printf("%d",b[I]);
+}
+while(ch!=4)
+{
+printf("\n********************MENU***************\n1.Union\n2.Intersection\n3.Complement\n4.exit\n\nENTER YOUR CHOICE");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:uni();
+break;
+case 2:inter();
+break;
+case 3:comp();
+break;
+case 4: 
+break;
+default:printf("Invalid choice");
+}
+}
+}
+
